151da3dc787fe521d96f0323c6ac426eef2e6a58
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-05-20 12:07:03 -0400

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,8 @@
+select first_name, last_name from public.employees
+
+/*
+-- filter on first name and last name to limimt the amount of date returned and focus
+-- and focus the filtering on a single person.
+
+*/
+where first_name = 'Mayumi' and last_name = 'Schueller';
